6f56f03e9bf0cb1e54bd8843e0456e75ec1ca979
r2d2rigo <r2d2rigo@gmail.com>
2015-02-15 01:27:51 +0000

    Changed Mercury.ParticleEngine.SharpDX.Direct3D11 back to normal library.

--- Mercury.ParticleEngine.SharpDX.Direct3D11.Shared/Resources.Designer.cs
+++ Mercury.ParticleEngine.SharpDX.Direct3D11.Shared/Resources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Mercury.ParticleEngine {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace Mercury.ParticleEngine {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mercury.ParticleEngine.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mercury.ParticleEngine.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
